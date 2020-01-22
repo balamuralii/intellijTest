2acf4239eb5ea65c506f6f3cec9b9ba776d95354
balamurali <Balamuralipati@outlook.com>
2020-01-22 22:53:25 +0530

    first commit

---
+++ src/test/java/com/zipkin/server/demo/ZipkinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipkin.server.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
